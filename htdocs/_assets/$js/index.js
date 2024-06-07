ad0208c98da8f049e8c7817283dafdfa6ae9a406
saran@linux <saranmass685@gmail.com>
2024-06-07 20:14:17 +0530

    completed home page

--- htdocs/_assets/$js/index.js
+++ htdocs/_assets/$js/index.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
+
+    let iconOpen = $('button.nav-icon-open');
+    let iconClose = $('button.nav-icon-close');
+
     $(window).on("scroll", function() {
         var header = $("header");
         header.toggleClass("sticky", $(this).scrollTop() > 0);
+    });
+
+    $(iconOpen).on('click', function() {
+        console.log('clicked');
+        $(iconOpen).toggleClass('active');
+    });
+
+    $(iconClose).on('click', function() {
+        $(iconClose).toggleClass('active');
     });
 });
--- htdocs/index.php
+++ htdocs/index.php
@@ -15,7 +15,7 @@ include 'libs/mid.php';
         <meta name="keywords" content="Techy Software, software solutions, business software, innovative software, software development">
         <meta name="author" content="Techy Software">
 
-        <!-- Open Graph Meta Tags for Social Media -->
+        <!-- toggel Graph Meta Tags for Social Media -->
         <meta property="og:title" content="Techy Software - Innovative Software Solutions">
         <meta property="og:description" content="Techy Software offers innovative software solutions for businesses of all sizes.">
         <meta property="og:image" content="https://example.com/images/og-image.jpg">
@@ -30,9 +30,9 @@ include 'libs/mid.php';
     </head>
     <body>
         <header class="header">
-            <h1 class="logo">Techy Software</h1>
-            <nav>
-                <ul>
+            <h1 class="logo m-0">Techy Software</h1>
+            <nav class="res_nav">
+                <ul class="res_list">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About Us</a></li>
                     <li><a href="#services">Services</a></li>
@@ -40,15 +40,23 @@ include 'libs/mid.php';
                     <li><a href="#contact">Contact Us</a></li>
                 </ul>
             </nav>
+            <button class="nav-icon-toggel position-relative z-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M2 18c0-1.54 0-2.31.347-2.876c.194-.317.46-.583.777-.777C3.689 14 4.46 14 6 14s2.31 0 2.876.347c.317.194.583.46.777.777C10 15.689 10 16.46 10 18s0 2.31-.347 2.877c-.194.316-.46.582-.777.776C8.311 22 7.54 22 6 22s-2.31 0-2.876-.347a2.35 2.35 0 0 1-.777-.776C2 20.31 2 19.54 2 18m12 0c0-1.54 0-2.31.347-2.876c.194-.317.46-.583.777-.777C15.689 14 16.46 14 18 14s2.31 0 2.877.347c.316.194.582.46.776.777C22 15.689 22 16.46 22 18s0 2.31-.347 2.877a2.36 2.36 0 0 1-.776.776C20.31 22 19.54 22 18 22s-2.31 0-2.876-.347a2.35 2.35 0 0 1-.777-.776C14 20.31 14 19.54 14 18M2 6c0-1.54 0-2.31.347-2.876c.194-.317.46-.583.777-.777C3.689 2 4.46 2 6 2s2.31 0 2.876.347c.317.194.583.46.777.777C10 3.689 10 4.46 10 6s0 2.31-.347 2.876c-.194.317-.46.583-.777.777C8.311 10 7.54 10 6 10s-2.31 0-2.876-.347a2.35 2.35 0 0 1-.777-.777C2 8.311 2 7.54 2 6m12 0c0-1.54 0-2.31.347-2.876c.194-.317.46-.583.777-.777C15.689 2 16.46 2 18 2s2.31 0 2.877.347c.316.194.582.46.776.777C22 3.689 22 4.46 22 6s0 2.31-.347 2.876c-.194.317-.46.583-.776.777C20.31 10 19.54 10 18 10s-2.31 0-2.876-.347a2.35 2.35 0 0 1-.777-.777C14 8.311 14 7.54 14 6" color="currentColor"/>
+                </svg>
+            </button>
         </header>
         
         <main>
             <section id="home" class="home-section">
-                <div>
-                    <h2>Welcome to Techy Software</h2>
-                    <p>The best software and website development company. we provides creative, strategic web design & web application and software development to make your aspiring customers friendly understand your products and services.</p>
+                <div class="container home-content d-flex flex-row align-items-center">
+                    <div class="mb-2">
+                        <h2>Welcome to Techy Software</h2>
+                        <p class="p-2">The best software and website development company. we're provides creative, strategic web design & web application and software development to make your aspiring customers friendly understand your products and services.</p>
+                        <a href="#about" type="button" class="btn btn-outline-primary position-relative z-1">About Us</a>
+                    </div>
+                    <img src="./_assets/$images/Avatar.png" class="image-layer" width="50%" height="50%" alt="Software interface Avatar" loading="lazy">
                 </div>
-                <img src="#" alt="Screenshot of our software interface" loading="lazy">
             </section>
 
             <section id="about" class="about-section">
@@ -95,4 +103,12 @@ include 'libs/mid.php';
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     </body>
     <script src="<?=getConfig('root_path');?>_assets/$js/index.js"></script>
+    <script>
+        var iconToggel = $('button.nav-icon-toggel');
+
+        $(iconToggel).on('click', function() {
+            console.log('clicked');
+            $('.res_nav, .res_list').toggleClass('active');
+        });
+    </script>
 </html>
