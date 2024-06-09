0db777297d2a51de8b6d6c5c807d0639826812fe
saran@linux <saranmass685@gmail.com>
2024-06-09 18:02:33 +0530

    implemented service section

--- htdocs/_templates/_sections/home.php
+++ htdocs/_templates/_sections/home.php
@@ -2,7 +2,7 @@
     <div class="container home-content d-flex flex-row align-items-center">
         <div class="mb-2 text-animy-1">
             <h2>Welcome to Techy Software</h2>
-            <p class="p-2 lh-lg">The best software and website development company. we're provides creative, strategic web design & web application and software development to make your aspiring customers friendly understand your products and services.</p>
+            <p class="p-2 lh-lg">The best software and website development company. we are provides creative, strategic web design & web application and software development to make your aspiring customers friendly understand your products and services.</p>
             <a href="#about" type="button" class="btn btn-outline-primary position-relative z-1">About Us</a>
         </div>
         <img src="./_assets/$images/Avatar.png" class="image-layer-1" width="50%" height="50%" alt="Software interface Avatar" loading="lazy">
--- htdocs/_templates/_sections/services.php
+++ htdocs/_templates/_sections/services.php
@@ -1,4 +1,35 @@
 <section id="services" class="services-section">
-    <h2>Our Services</h2>
-    <p>We offer a wide range of services including custom software development, software consulting, and support and maintenance services.</p>
+    <div class="container services-content d-flex flex-column align-items-center">
+        <h2 class="m-0 mt-4">Our Services</h2>
+        <!-- Cards -->
+        <div class="row mt-5 mt-md-4 row-cols-1 row-cols-sm-1 row-cols-md-3 justify-content-center">
+            <div class="col pb-3">
+                <div class="service-card">
+                    <div class="icon-wrapper">
+                        <img src="https://cdn-icons-png.flaticon.com/512/2210/2210153.png" width="100%" alt="Website Development">
+                    </div>
+                    <h3>Website Development</h3>
+                    <p>We are provides creative, strategic web design & web application development to make your aspiring customers friendly understand your products and services.</p>
+                </div>
+            </div>
+            <div class="col pb-3">
+                <div class="service-card">
+                    <div class="icon-wrapper">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGRgThDdgw9IyK2wWQLK8t4TlaeUeAiPzfDQ&s" width="100%" alt="Mobile App Development">
+                    </div>
+                    <h3>Mobile App Development</h3>
+                    <p>We are providing throughout solution in mobile app development. We develop the mobile apps including Android and IPhone app development.</p>
+                </div>
+            </div>
+            <div class="col pb-3">
+                <div class="service-card">
+                    <div class="icon-wrapper">
+                    <img src="https://img.freepik.com/premium-vector/digital-marketing-icon-vector_946691-923.jpg" width="100%" alt="Digital Marketing">
+                    </div>
+                    <h3>Digital Marketing</h3>
+                    <p>We are providing online marketing agency promote your website across all the digital platforms using expert techniques and creative methods to drive more traffics.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
